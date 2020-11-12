370726a6fcd1d9d0c4c77ed1f22ee776600fd287
Annelise <annelzz26@gmail.com>
2020-11-11 19:34:28 -0800

    commit tree 40673660e26ca66e78399f3224adc990c95249db

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,1 +1,32 @@
-# Write your code here.
+def badge_maker(name)
+    return "Hello, my name is #{name}."
+end
+def batch_badge_creator(names)
+    message = []
+    names.each do |name|
+         message << "Hello, my name is #{name}."
+    end
+    return message
+end    
+def assign_rooms(speakers)
+    room_greetings = []
+    speakers.each_with_index.collect do |speaker, index|
+        room_greetings << "Hello, #{speaker}! You'll be assigned to room #{index + 1}!"
+    end
+    return room_greetings
+end    
+def printer(names)
+     batch_badge_creator(names).each do |names|
+        puts names
+     end
+     assign_rooms(names).each do |names|
+        puts names
+     end
+
+
+end    
+
+
+
+        
+
